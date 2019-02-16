3105ca1597dc98604edb711127123cd3dfdaf78b
MOZGIII <mike-n@narod.ru>
2019-02-16 11:32:11 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,53 @@
+FROM rust:1.32-stretch AS builder
+
+RUN echo "deb http://deb.debian.org/debian stretch-backports main" >> /etc/apt/sources.list \
+ && cat /etc/apt/sources.list \
+ && apt-get update \
+ && apt-get install -y --no-install-recommends \
+      -t stretch-backports \
+      "clang-6.0" \
+      "build-essential" \
+      "libsodium-dev" \
+ && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /usr/src/app
+
+COPY Cargo.toml Cargo.lock ./
+# Create a fake `src`, build deps and remove the fake `src`.
+# See https://github.com/rust-lang/cargo/issues/2644.
+RUN mkdir src \
+ && touch src/lib.rs \
+ && cargo build --locked --release \
+ && rm -rf src
+
+COPY src src
+RUN ls -la \
+ && cargo build --frozen --release \
+ && ldd target/release/musicbot
+
+FROM debian:stretch
+
+RUN echo "deb http://deb.debian.org/debian stretch-backports main" >> /etc/apt/sources.list \
+ && cat /etc/apt/sources.list \
+ && apt-get update \
+ && apt-get install -y --no-install-recommends \
+      -t stretch-backports \
+      "libsodium23" \
+      "libssl1.1" \
+      "libcrypto++6" \
+      "ca-certificates" \
+      "ffmpeg" \
+      "youtube-dl" \
+      "aria2" \
+      "curl" \
+      "wget" \
+      "rtmpdump" \
+      "phantomjs" \
+      "python3-pyxattr" \
+ && rm -rf /var/lib/apt/lists/*
+
+COPY --from=builder /usr/src/app/target/release/musicbot /usr/local/bin/musicbot
+
+RUN ["ldd", "/usr/local/bin/musicbot"]
+
+CMD ["musicbot"]
---
+++ src/main.rs
@@ -0,0 +1,286 @@
+#[macro_use] extern crate serenity;
+
+use std::{env, sync::Arc};
+
+use serenity::client::bridge::voice::ClientVoiceManager;
+use serenity::{client::{Context}, prelude::Mutex};
+
+use serenity::{
+    client::{CACHE, Client, EventHandler},
+    framework::StandardFramework,
+    model::{channel::Message, gateway::Ready, misc::Mentionable},
+    Result as SerenityResult,
+    voice,
+};
+
+// This imports `typemap`'s `Key` as `TypeMapKey`.
+use serenity::prelude::*;
+
+struct VoiceManager;
+
+impl TypeMapKey for VoiceManager {
+    type Value = Arc<Mutex<ClientVoiceManager>>;
+}
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    // Configure the client with your Discord bot token in the environment.
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a DISCORD_TOKEN in the environment");
+    let mut client = Client::new(&token, Handler)?;
+
+    // Obtain a lock to the data owned by the client, and insert the client's
+    // voice manager into it. This allows the voice manager to be accessible by
+    // event handlers and framework commands.
+    {
+        let mut data = client.data.lock();
+        data.insert::<VoiceManager>(Arc::clone(&client.voice_manager));
+    }
+
+    client.with_framework(StandardFramework::new()
+        .configure(|c| c
+            .prefix("~")
+            .on_mention(true))
+        .cmd("deafen", deafen)
+        .cmd("join", join)
+        .cmd("leave", leave)
+        .cmd("mute", mute)
+        .cmd("play", play)
+        .cmd("ping", ping)
+        .cmd("undeafen", undeafen)
+        .cmd("unmute", unmute));
+
+    let _ = client.start().map_err(|why| println!("Client ended: {:?}", why));
+    Ok(())
+}
+
+command!(deafen(ctx, msg) {
+    let guild_id = match CACHE.read().guild_channel(msg.channel_id) {
+        Some(channel) => channel.read().guild_id,
+        None => {
+            check_msg(msg.channel_id.say("Groups and DMs not supported"));
+
+            return Ok(());
+        },
+    };
+
+    let mut manager_lock = ctx.data.lock().get::<VoiceManager>().cloned().unwrap();
+    let mut manager = manager_lock.lock();
+
+    let handler = match manager.get_mut(guild_id) {
+        Some(handler) => handler,
+        None => {
+            check_msg(msg.reply("Not in a voice channel"));
+
+            return Ok(());
+        },
+    };
+
+    if handler.self_deaf {
+        check_msg(msg.channel_id.say("Already deafened"));
+    } else {
+        handler.deafen(true);
+
+        check_msg(msg.channel_id.say("Deafened"));
+    }
+});
+
+command!(join(ctx, msg) {
+    let guild = match msg.guild() {
+        Some(guild) => guild,
+        None => {
+            check_msg(msg.channel_id.say("Groups and DMs not supported"));
+
+            return Ok(());
+        }
+    };
+
+    let guild_id = guild.read().id;
+
+    let channel_id = guild
+        .read()
+        .voice_states.get(&msg.author.id)
+        .and_then(|voice_state| voice_state.channel_id);
+
+
+    let connect_to = match channel_id {
+        Some(channel) => channel,
+        None => {
+            check_msg(msg.reply("Not in a voice channel"));
+
+            return Ok(());
+        }
+    };
+
+    let mut manager_lock = ctx.data.lock().get::<VoiceManager>().cloned().expect("Expected VoiceManager in ShareMap.");
+    let mut manager = manager_lock.lock();
+
+    if manager.join(guild_id, connect_to).is_some() {
+        check_msg(msg.channel_id.say(&format!("Joined {}", connect_to.mention())));
+    } else {
+        check_msg(msg.channel_id.say("Error joining the channel"));
+    }
+});
+
+command!(leave(ctx, msg) {
+    let guild_id = match CACHE.read().guild_channel(msg.channel_id) {
+        Some(channel) => channel.read().guild_id,
+        None => {
+            check_msg(msg.channel_id.say("Groups and DMs not supported"));
+
+            return Ok(());
+        },
+    };
+
+    let mut manager_lock = ctx.data.lock().get::<VoiceManager>().cloned().expect("Expected VoiceManager in ShareMap.");
+    let mut manager = manager_lock.lock();
+    let has_handler = manager.get(guild_id).is_some();
+
+    if has_handler {
+        manager.remove(guild_id);
+
+        check_msg(msg.channel_id.say("Left voice channel"));
+    } else {
+        check_msg(msg.reply("Not in a voice channel"));
+    }
+});
+
+command!(mute(ctx, msg) {
+    let guild_id = match CACHE.read().guild_channel(msg.channel_id) {
+        Some(channel) => channel.read().guild_id,
+        None => {
+            check_msg(msg.channel_id.say("Groups and DMs not supported"));
+
+            return Ok(());
+        },
+    };
+
+    let mut manager_lock = ctx.data.lock().get::<VoiceManager>().cloned().expect("Expected VoiceManager in ShareMap.");
+    let mut manager = manager_lock.lock();
+
+    let handler = match manager.get_mut(guild_id) {
+        Some(handler) => handler,
+        None => {
+            check_msg(msg.reply("Not in a voice channel"));
+
+            return Ok(());
+        },
+    };
+
+    if handler.self_mute {
+        check_msg(msg.channel_id.say("Already muted"));
+    } else {
+        handler.mute(true);
+
+        check_msg(msg.channel_id.say("Now muted"));
+    }
+});
+
+command!(ping(_context, msg) {
+    check_msg(msg.channel_id.say("Pong!"));
+});
+
+command!(play(ctx, msg, args) {
+    let url = match args.single::<String>() {
+        Ok(url) => url,
+        Err(_) => {
+            check_msg(msg.channel_id.say("Must provide a URL to a video or audio"));
+
+            return Ok(());
+        },
+    };
+
+    if !url.starts_with("http") {
+        check_msg(msg.channel_id.say("Must provide a valid URL"));
+
+        return Ok(());
+    }
+
+    let guild_id = match CACHE.read().guild_channel(msg.channel_id) {
+        Some(channel) => channel.read().guild_id,
+        None => {
+            check_msg(msg.channel_id.say("Error finding channel info"));
+
+            return Ok(());
+        },
+    };
+
+    let mut manager_lock = ctx.data.lock().get::<VoiceManager>().cloned().expect("Expected VoiceManager in ShareMap.");
+    let mut manager = manager_lock.lock();
+
+    if let Some(handler) = manager.get_mut(guild_id) {
+        let source = match voice::ytdl(&url) {
+            Ok(source) => source,
+            Err(why) => {
+                println!("Err starting source: {:?}", why);
+
+                check_msg(msg.channel_id.say("Error sourcing ffmpeg"));
+
+                return Ok(());
+            },
+        };
+
+        handler.play(source);
+
+        check_msg(msg.channel_id.say("Playing song"));
+    } else {
+        check_msg(msg.channel_id.say("Not in a voice channel to play in"));
+    }
+});
+
+command!(undeafen(ctx, msg) {
+    let guild_id = match CACHE.read().guild_channel(msg.channel_id) {
+        Some(channel) => channel.read().guild_id,
+        None => {
+            check_msg(msg.channel_id.say("Error finding channel info"));
+
+            return Ok(());
+        },
+    };
+
+    let mut manager_lock = ctx.data.lock().get::<VoiceManager>().cloned().expect("Expected VoiceManager in ShareMap.");
+    let mut manager = manager_lock.lock();
+
+    if let Some(handler) = manager.get_mut(guild_id) {
+        handler.deafen(false);
+
+        check_msg(msg.channel_id.say("Undeafened"));
+    } else {
+        check_msg(msg.channel_id.say("Not in a voice channel to undeafen in"));
+    }
+});
+
+command!(unmute(ctx, msg) {
+    let guild_id = match CACHE.read().guild_channel(msg.channel_id) {
+        Some(channel) => channel.read().guild_id,
+        None => {
+            check_msg(msg.channel_id.say("Error finding channel info"));
+
+            return Ok(());
+        },
+    };
+    let mut manager_lock = ctx.data.lock().get::<VoiceManager>().cloned().expect("Expected VoiceManager in ShareMap.");
+    let mut manager = manager_lock.lock();
+
+    if let Some(handler) = manager.get_mut(guild_id) {
+        handler.mute(false);
+
+        check_msg(msg.channel_id.say("Unmuted"));
+    } else {
+        check_msg(msg.channel_id.say("Not in a voice channel to undeafen in"));
+    }
+});
+
+/// Checks that a message successfully sent; if not, then logs why to stdout.
+fn check_msg(result: SerenityResult<Message>) {
+    if let Err(why) = result {
+        println!("Error sending message: {:?}", why);
+    }
+}
