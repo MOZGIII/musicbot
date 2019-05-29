5dbf23b0ae3b4f92e4a6afe7b1c16724008db0b6
MOZGIII <mike-n@narod.ru>
2019-05-30 00:29:36 +0300

    Removed custom help command to reduce code footprint

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,12 @@
-use std::collections::HashSet;
 use std::env;
 
-use serenity::{
-    client::{Context, Client},
-    framework::standard::{
-        HelpOptions,
-        help_commands,
-        Args,
-        CommandResult,
-        CommandGroup,
-        StandardFramework,
-        macros::help,
-    },
-    model::{channel::Message, id::UserId},
-};
+use serenity::{client::Client, framework::standard::StandardFramework};
 
 mod standard_framerork_config;
 use standard_framerork_config::StandardFrameworkConfig;
 mod commands;
 mod handler;
 mod voice_manager;
-
-#[help]
-#[individual_command_tip =
-"Hello! こんにちは！Hola! Bonjour! 您好!\n\
-If you want more information about a specific command, just pass the command as argument."]
-#[command_not_found_text = "Could not find: `{}`."]
-fn bot_help(
-    context: &mut Context,
-    msg: &Message,
-    args: Args,
-    help_options: &'static HelpOptions,
-    groups: &[&'static CommandGroup],
-    owners: HashSet<UserId>
-) -> CommandResult {
-    help_commands::with_embeds(context, msg, args, help_options, groups, owners)
-}
 
 fn main() -> Result<(), Box<std::error::Error>> {
     let token = env::var("DISCORD_TOKEN").expect("Expected a DISCORD_TOKEN in the environment");
@@ -48,7 +19,6 @@ fn main() -> Result<(), Box<std::error::Error>> {
     client.with_framework(
         StandardFramework::new()
             .configure(|c| configurator.configure(c))
-            .help(&BOT_HELP_HELP_COMMAND)
             .group(&commands::voice::VOICE_GROUP)
             .group(&commands::system::SYSTEM_GROUP)
             .group(&commands::experimental::EXPERIMENTAL_GROUP)
